812a5dcca54192219fc74105c9efec53dab11ada
Willi Ye <williye97@gmail.com>
2014-08-26 13:34:31 +0200

    comfig dump, makefile flags and use lzo for ramdisk
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- ramdisk/build.sh
+++ ramdisk/build.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-rm -rf kernel.zip
-rm -rf ramdisk.gz
-find -name "*~" -exec rm -rf {} \;
-find -name ".DS_Store" -exec rm -rf {} \;
+rm -f kernel.zip
+rm -f ramdisk.lzo
+find -name "*~" -exec rm -f {} \;
+find -name ".DS_Store" -exec rm -f {} \;
 
 build () {
 	cd boot.img-ramdisk
-	find . | cpio -o -H newc | gzip > ../ramdisk.gz
+	find . | cpio -o -H newc | lzop > ../ramdisk.lzo
 	cd ..
-	./mkbootimg-$1 --kernel zImage --base 0x00000000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000 --pagesize 2048 --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 zswap.enabled=0 zswap.compressor=snappy" --ramdisk ramdisk.gz --dt dt.img -o boot.img
+	./mkbootimg-$1 --kernel zImage --base 0x00000000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000 --pagesize 2048 --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 zswap.enabled=0 zswap.compressor=snappy" --ramdisk ramdisk.lzo --dt dt.img -o boot.img
 }
 
-rm -f boot.img-ramdisk/lib/modules/wp_mod.ko
 ./dtbToolCM -s 2048 -d "htc,project-id = <" -o dt.img -p ../scripts/dtc/ ../arch/arm/boot/
 
 if [ -e ~/.bash_profile ]; then

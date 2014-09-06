03f93def410241d7ef54489fd8bd8fc62baaf4e8
Willi Ye <williye97@gmail.com>
2014-09-06 16:10:07 +0200

    ramdisk: clean up gk.sh
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- ramdisk/boot.img-ramdisk/sbin/gk.sh
+++ ramdisk/boot.img-ramdisk/sbin/gk.sh
@@ -3,10 +3,6 @@
 mount -o remount,rw /system
 /sbin/busybox mount -t rootfs -o remount,rw rootfs
 
-echo 2 > /sys/devices/system/cpu/sched_mc_power_savings
-
-for i in /sys/block/*/queue/add_random;do echo 0 > $i;done
-
 ln -s /res/synapse/uci /sbin/uci
 /sbin/uci
 

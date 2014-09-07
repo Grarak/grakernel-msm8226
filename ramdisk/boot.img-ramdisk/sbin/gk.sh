e28208b175091303392bba26261134e9e3b8cc1f
Willi Ye <williye97@gmail.com>
2014-09-07 12:00:03 +0200

    ramdisk: use gzip
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- ramdisk/boot.img-ramdisk/sbin/gk.sh
+++ ramdisk/boot.img-ramdisk/sbin/gk.sh
@@ -1,12 +1,31 @@
 #!/sbin/busybox sh
 
-mount -o remount,rw /system
+/sbin/busybox mount -o remount,rw /system
 /sbin/busybox mount -t rootfs -o remount,rw rootfs
+
+cd /sbin
+
+for i in $(./busybox --list)
+do
+	./busybox ln -s busybox $i
+done
+
+cd /
+
+mv -f /res/sense44/* /
+
+rm -rf /system/lib/modules/*
+ln -s /lib/modules/* /system/lib/modules/
+
+mv -f /res/wifi/prima/* /system/etc/firmware/wlan/prima
+mv -f /res/wifi/*.conf /system/etc/wifi
+
+[ -f /system/app/Synapse.apk ] || mv -f /res/synapse/Synapse.apk /system/app
 
 ln -s /res/synapse/uci /sbin/uci
 /sbin/uci
 
 [ -d /system/etc/init.d ] && /sbin/busybox run-parts /system/etc/init.d
 
-/sbin/busybox mount -t rootfs -o remount,ro rootfs
+mount -t rootfs -o remount,ro rootfs
 mount -o remount,ro /system
--- ramdisk/build.sh
+++ ramdisk/build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 rm -f kernel.zip
-rm -f ramdisk.lzo
+rm -f ramdisk.gz
 find -name "*~" -exec rm -f {} \;
 find -name ".DS_Store" -exec rm -f {} \;
 
 build () {
 	cd boot.img-ramdisk
-	find . | cpio -o -H newc | lzop > ../ramdisk.lzo
+	find . | cpio -o -H newc | gzip -9 > ../ramdisk.gz
 	cd ..
-	./mkbootimg-$1 --kernel zImage --base 0x00000000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000 --pagesize 2048 --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 zswap.enabled=0 zswap.compressor=snappy" --ramdisk ramdisk.lzo --dt dt.img -o boot.img
+	./mkbootimg-$1 --kernel zImage --base 0x00000000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000 --pagesize 2048 --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 zswap.enabled=0 zswap.compressor=snappy" --ramdisk ramdisk.gz --dt dt.img -o boot.img
 }
 
 ./dtbToolCM -s 2048 -d "htc,project-id = <" -o dt.img -p ../scripts/dtc/ ../arch/arm/boot/

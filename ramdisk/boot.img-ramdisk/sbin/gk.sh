0eda1051069b80d572c5f0b593786f84e6c937bf
Willi Ye <williye97@gmail.com>
2015-01-29 20:52:44 +0100

    ramdisk: Fix UCI permissions

--- ramdisk/boot.img-ramdisk/sbin/gk.sh
+++ ramdisk/boot.img-ramdisk/sbin/gk.sh
@@ -1,7 +1,7 @@
-#!/system/bin/sh
+#!/system/xbin/busybox sh
 
+mount -o rw,remount /
 mount -o rw,remount /system
-mount -t rootfs -o rw,remount rootfs
 
 mkdir -p /system/lib/modules/backup
 mv -f /system/lib/modules/*.ko /system/lib/modules/backup
@@ -12,14 +12,14 @@ if [ ! -d /system/app/Synapse ]; then
 	cp -f /res/synapse/Synapse.apk /system/app/Synapse/Synapse.apk
 fi
 
-chmod 777 /res/synapse/uci
-chmod 777 /res/synapse/*
-chmod 777 /res/synapse/actions/*
+chmod 755 /res/synapse/uci
+chmod 755 /res/synapse/*
+chmod 755 /res/synapse/actions/*
 
 ln -s /res/synapse/uci /sbin/uci
 /sbin/uci
 
 [ -d /system/etc/init.d ] && /sbin/busybox run-parts /system/etc/init.d
 
-mount -t rootfs -o ro,remount rootfs
 mount -o ro,remount /system
+mount -o ro,remount /

c6269bc317e2802ab67ac9f28534272f544034f0
Willi Ye <williye97@gmail.com>
2015-03-13 22:27:26 +0100

    ramdisk: update

--- ramdisk/build.sh
+++ ramdisk/build.sh
@@ -7,7 +7,7 @@ find -name ".DS_Store" -exec rm -f {} \;
 
 build () {
 	./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
-	./mkbootimg-$1 --kernel zImage --base 0x00000000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000 --pagesize 2048 --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 zcache androidboot.bootdevice=msm_sdcc.1 zswap.enabled=0 zswap.compressor=snappy" --ramdisk ramdisk.gz --dt dt.img -o boot.img
+	./mkbootimg-$1 --kernel zImage --base 0x00000000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000 --pagesize 2048 --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 zcache androidboot.bootdevice=msm_sdcc.1 zswap.enabled=0 zswap.compressor=snappy androidboot.selinux=permissive" --ramdisk ramdisk.gz --dt dt.img -o boot.img
 }
 
 ./dtbToolCM -s 2048 -d "htc,project-id = <" -o dt.img -p ../scripts/dtc/ ../arch/arm/boot/
